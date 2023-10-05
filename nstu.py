a51ccf2b6caff9e29ed668c586228ccc58c098e5
KysЬ <kirill.veber.81@mail.ru>
2023-10-05 21:11:52 +0700

    with day and type work

--- nstu.py
+++ nstu.py
@@ -7,9 +7,18 @@ def get_schedule(url):
     soup = BeautifulSoup(req.text, 'html.parser')
 
     schedule_data = []
+    current_day = None  # Переменная для отслеживания текущего дня
 
     class_rows = soup.find_all('div', class_='schedule__table-row')
+
     for class_row in class_rows:
+        day_rows = class_row.find('div', class_='schedule__table-day')
+        day_text = day_rows.get_text(strip=True) if day_rows else ''
+
+        # Если день изменился, обновляем current_day
+        if day_text:
+            current_day = day_text
+
         class_data = class_row.find_all('div', class_='schedule__table-cell')
         if len(class_data) < 2:
             continue
@@ -20,21 +29,26 @@ def get_schedule(url):
         else:
             time = ''
 
-        item = class_data[1].find('div', attrs={'data-type': 'item'})
+        item = class_data[1].find('div', class_='schedule__table-cell')
 
         if item:
-            subject = item.get_text(strip=True)
+            subject_element = item.find('div', class_='schedule__table-item')
+            work_type = item.find('span', class_='schedule__table-typework')
+            subject_text = subject_element.get_text(strip=True) if subject_element else ''
+            work_type_text = work_type.get_text(strip=True) if work_type else ''
             classroom_element = item.find('span', class_='schedule__table-class')
-            if classroom_element:
-                classroom = classroom_element.get_text(strip=True)
-            else:
-                classroom = ''
+            classroom_text = classroom_element.get_text(strip=True) if classroom_element else ''
+            subject = subject_text.replace(classroom_text, '').replace(work_type_text, '').strip()
+            classroom = classroom_text
+            type_work = work_type_text
         else:
-            subject, classroom = '', ''
+            subject, classroom, type_work = '', '', ''
 
         class_schedule = {
+            'day': current_day,  # Используем текущий день
             'time': time,
             'subject': subject,
+            'type': type_work,
             'classroom': classroom
         }
 
@@ -48,11 +62,18 @@ def main():
     group = input()
     url = f'https://www.nstu.ru/studies/schedule/schedule_classes/schedule?group={group}'
     schedule = get_schedule(url)
+    current_day = None  # Переменная для отслеживания текущего дня
 
     for class_schedule in schedule:
+        # Проверяем, изменился ли день
+        if class_schedule['day'] != current_day:
+            print(f"День: {class_schedule['day']}")
+            current_day = class_schedule['day']
+
         if (class_schedule['time'] and class_schedule['subject'] and class_schedule['classroom']):
             print(f"Время: {class_schedule['time']}")
             print(f"Предмет: {class_schedule['subject']}")
+            print(f"Тип пары: {class_schedule['type']}")
             print(f"Аудитория: {class_schedule['classroom']}")
             print('-' * 50)
 
